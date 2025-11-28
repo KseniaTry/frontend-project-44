857fa5d9e0710f254d46d8e58da7eeda94dbfb94
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2025-11-28 15:16:11 +0300

    add brain-gcd game logics

---
+++ src/games/gcd.js
@@ -0,0 +1,27 @@
+import initGame from '../index.js'
+import { initGreeting } from '../cli.js'
+import { getRandomInRange } from '../util.js'
+import { MIN_RANDOM, MAX_RANDOM } from '../const.js'
+
+const getExpression = () => `${getRandomInRange(MIN_RANDOM, MAX_RANDOM)} ${getRandomInRange(MIN_RANDOM, MAX_RANDOM)}`
+
+const answerCheck = (expression) => {
+  const modifiedExpression = expression.split(' ')
+  let first = modifiedExpression[0]
+  let second = modifiedExpression[1]
+
+  while (second !== 0) {
+    const defaultdFirst = first
+    first = second
+    second = defaultdFirst % second
+  }
+  return first
+}
+
+const run = () => {
+  initGreeting()
+  console.log('Find the greatest common divisor of given numbers.')
+  initGame(getExpression, answerCheck)
+}
+
+export default run
